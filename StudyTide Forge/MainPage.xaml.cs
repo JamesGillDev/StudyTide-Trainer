577ab21cd2fc55c44b89bd427937cd971e808058
James Gill <echoing1822tide@outlook.com>
2026-03-02 12:31:21 -0700

    Stabilize NewIcon launcher by syncing publish alias and webview data path

--- StudyTide Forge/MainPage.xaml.cs
+++ StudyTide Forge/MainPage.xaml.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.Storage;
+
 namespace StudyTideForge;
 
 public partial class MainPage : ContentPage
@@ -5,5 +9,13 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        blazorWebView.BlazorWebViewInitializing += OnBlazorWebViewInitializing;
+    }
+
+    private static void OnBlazorWebViewInitializing(object? sender, BlazorWebViewInitializingEventArgs args)
+    {
+        var userDataFolder = Path.Combine(FileSystem.AppDataDirectory, "webview2");
+        Directory.CreateDirectory(userDataFolder);
+        args.UserDataFolder = userDataFolder;
     }
 }
